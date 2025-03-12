07a6854bd70420652ffdca7b3e85c10a4db07f70
yuqing-tham <yuqing.tham@gmail.com>
2025-03-13 01:10:09 +0800

    Add healthcare staff support

---
+++ src/main/java/seedu/address/model/person/ProviderRoleType.java
@@ -0,0 +1,10 @@
+package seedu.address.model.person;
+
+/**
+ * Represents the different roles of the healthcare providers.
+ */
+public enum ProviderRoleType {
+    DOCTOR,
+    NURSE,
+    THERAPIST
+}
