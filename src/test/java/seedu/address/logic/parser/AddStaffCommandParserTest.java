5d0f836d7b8b9767ea9804d4bef6bc7ede04baaa
Siyan-G <gsy7418@gmail.com>
2025-03-20 22:54:36 +0800

    Fix import order for checkstyle

--- src/test/java/seedu/address/logic/parser/AddStaffCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddStaffCommandParserTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Set;
 
