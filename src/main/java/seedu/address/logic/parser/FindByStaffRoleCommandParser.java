8eec1087fc40ef9e31c13f8ae14f37e0f82b2dc9
yuqing-tham <yuqing.tham@gmail.com>
2025-03-23 18:06:03 +0800

    Add support for findstaff command

---
+++ src/main/java/seedu/address/logic/parser/FindByStaffRoleCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindByStaffRoleCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.ProviderRoleContainsKeywordPredicate;
+
+/**
+ * Parses input arguments and creates a new FindByStaffRoleCommand object.
+ */
+public class FindByStaffRoleCommandParser implements Parser<FindByStaffRoleCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindByStaffRoleCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        String[] roleKeywords = trimmedArgs.split("\\s+");
+
+        return new FindByStaffRoleCommand(new ProviderRoleContainsKeywordPredicate(Arrays.asList(roleKeywords)));
+    }
+}
